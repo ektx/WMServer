686b5785dd3f410881a8dfd711730eefbf534eeb
ektx <530675800@qq.com>
2018-06-08 08:53:05 +0800

    使用解构

--- bin/router.js
+++ bin/router.js
@@ -4,13 +4,13 @@ const router = express.Router()
 const tokenAuth = require('./tokenAuth')
 const { uploadFields, uploadCallback } = require('./upload')
 const displayFile = require('./displayFile')
-const routerEvt = require('../API/user/routerEvt')
+const { GetLogin, login } = require('../API/user/routerEvt')
 const saveArticle = require('../API/todoList/RESTful/saveArticle')
 
-router.get('/login', routerEvt.GetLogin)
+router.get('/login', GetLogin)
 router.get(/img|v|m/i, displayFile)
 
-router.post('/login', routerEvt.login)
+router.post('/login', login)
 router.post('/api/v1/todoEvent/saveArticle', tokenAuth, saveArticle)
 router.post('/upload', uploadCallback)
 
