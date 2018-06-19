271bc7e83e4c7cf886e934ee4da35760e6a4f7a5
ektx <530675800@qq.com>
2018-06-19 10:34:24 +0800

    优化返回数据的灵活性

--- API/res2.js
+++ API/res2.js
@@ -4,9 +4,13 @@ const {
     GraphQLBoolean,
 } = require('graphql')
 
-
-module.exports = function(name = 'resFeedbackBaseMod2', obj) {
-
+/**
+ * 
+ * @param {string} name 请求名称
+ * @param {object} obj 数据内容
+ * @param {boolean} useData 使用 Data 格式
+ */
+module.exports = function(name = 'resFeedbackBaseMod2', obj, useData) {
     let def = {
         success: {
             type: GraphQLBoolean,
@@ -17,13 +21,21 @@ module.exports = function(name = 'resFeedbackBaseMod2', obj) {
             description: '说明信息'
         }
     }
+    let fileds = {
+        ...def,
+        ...obj
+    }
+
+    if (useData) {
+        fileds = {
+            ...def,
+            data: obj
+        }
+    }
 
     return new GraphQLObjectType({
         name,
         description: '默认返回基础请求格式',
-        fields: () => ({
-            ...def,
-            ...obj
-        })
+        fields: () => (fileds)
     })
 }
